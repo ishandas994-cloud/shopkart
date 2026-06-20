fa163764374942f4cc8845aaf6667a2ba5e5370d
ishandas994-cloud <ishandas994@gmail.com>
2026-06-20 20:16:24 +0530

    new codes

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "/api",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("shopkart_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+API.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem("shopkart_token");
+      localStorage.removeItem("shopkart_user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default API;
